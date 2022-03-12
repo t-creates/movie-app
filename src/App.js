0c2605c19127cc3a8b3054da5b44a654f14c60bb
Travis Geislinger <travisgeislinger@Traviss-MacBook-Pro.local>
2022-03-12 13:30:44 -0700

    MovieBuff0

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import './App.css';
 
 const API_URL = 'http://www.omdbapi.com?apikey=7a20458a';
 
-const movie1 = {
-  "Title": "Batman Begins",
-  "Year": "2005",
-  "imdbID": "tt0372784",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-}
+// const movie1 = {
+//   "Title": "Batman Begins",
+//   "Year": "2005",
+//   "imdbID": "tt0372784",
+//   "Type": "movie",
+//   "Poster": "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+// }
 
 const App = () => {
   const [movies, setMovies] = useState([]);
